ff50fa8834559b4b9fc2a1dc5914811b74764d1c
vanmanh2030 <42065920+vanmanh2030@users.noreply.github.com>
2019-01-23 10:32:31 +0700

    Update bannerdautrang.js

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -117,7 +117,169 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='https://bit.ly/2LQqJje' target='_blank'><img src='https://lh4.googleusercontent.com/-Lx-8Qcc2gwI/W2QExugiXXI/AAAAAAAACXM/whvp4u6bozcmXsDMovOrVcHmadcMZUX8wCLcBGAs/s640/a5127b465ba2bafce3b3.jpg' width='600px'/><br/>Đất Nền Mặt Tiền Đường Hòa Bình</a>.Shophouse Richstar Novaland. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='https://bit.ly/2LQqJje' target='_blank'><img src='//please contact me giaiphapthuonghieu.org@gmail.com
+//website http://giaiphapthuonghieu.vn or http://faceseo.vn
+
+(function($) {
+    $.fn.linhnguyen = function(options) {        
+        var defaults = {  
+	    	animation: 'fadeAndPop', //fade, fadeAndPop, none
+		    animationspeed: 300, //how fast animtions are
+		    closeonbackgroundclick: true, //if you click background will modal close?
+		    dismissmodalclass: 'close-linhnguyen-modal' //the class of a button or element that will close an open modal
+    	}; 
+
+        var options = $.extend({}, defaults, options); 
+	
+        return this.each(function() {
+        	var modal = $(this),
+        		topMeasure  = parseInt(modal.css('top')),
+				topOffset = modal.height() + topMeasure,
+          		locked = false,
+				modalBG = $('.linhnguyen-modal-bg');
+
+			if(modalBG.length == 0) {
+				modalBG = $('<div class="linhnguyen-modal-bg" />').insertAfter(modal);
+			}		    
+			modal.bind('linhnguyen:open', function () {
+			  modalBG.unbind('click.modalEvent');
+				$('.' + options.dismissmodalclass).unbind('click.modalEvent');
+				if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modal.css({'top': $(document).scrollTop()-topOffset, 'opacity' : 0, 'visibility' : 'visible'});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"top": $(document).scrollTop()+topMeasure + 'px',
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					}
+					if(options.animation == "fade") {
+						modal.css({'opacity' : 0, 'visibility' : 'visible', 'top': $(document).scrollTop()+topMeasure});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					} 
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'visible', 'top':$(document).scrollTop()+topMeasure});
+						modalBG.css({"display":"block"});	
+						unlockModal()				
+					}
+				}
+				modal.unbind('linhnguyen:open');
+			}); 	
+
+			modal.bind('linhnguyen:close', function () {
+			  if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"top":  $(document).scrollTop()-topOffset + 'px',
+							"opacity" : 0
+						}, options.animationspeed/2, function() {
+							modal.css({'top':topMeasure, 'opacity' : 1, 'visibility' : 'hidden'});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "fade") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"opacity" : 0
+						}, options.animationspeed, function() {
+							modal.css({'opacity' : 1, 'visibility' : 'hidden', 'top' : topMeasure});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'hidden', 'top' : topMeasure});
+						modalBG.css({'display' : 'none'});	
+					}		
+				}
+				modal.unbind('linhnguyen:close');
+			});     
+   	
+    	modal.trigger('linhnguyen:open')
+			
+			//Close Modal Listeners
+			var closeButton = $('.' + options.dismissmodalclass).bind('click.modalEvent', function () {
+			  modal.trigger('linhnguyen:close')
+			});
+			
+			if(options.closeonbackgroundclick) {
+				modalBG.css({"cursor":"pointer"})
+				modalBG.bind('click.modalEvent', function () {
+				  modal.trigger('linhnguyen:close')
+				});
+			}
+			$('body').keyup(function(e) {
+        		if(e.which===27){ modal.trigger('linhnguyen:close'); } // 27 is the keycode for the Escape key
+			});
+			
+			function unlockModal() { 
+				locked = false;
+			}
+			function lockModal() {
+				locked = true;
+			}	
+			
+        });//each call
+    }//orbit plugin call
+})(jQuery);
+        
+$(window).load(function() {
+		if(document.cookie.indexOf("adf") == -1)
+		{
+			document.cookie = "adfpopunder1=adf";
+			var divpopup = document.createElement("div");
+			$(divpopup).attr('id', 'myModal');
+			$(divpopup).attr('class', 'linhnguyen-modal');
+			$( "body" ).append(divpopup);
+			$("#myModal").html("<a href='https://bit.ly/2LQqJje' target='_blank'><img src='<div style='background-image: url(https://2.bp.blogspot.com/-3HMsOb32PJk/XEcF-Hx828I/AAAAAAAAAGo/_5xiDWXejnIt-rBSJf-hH3ekW-ilDGslACLcBGAs/s1600/7.jpg); background-repeat: no-repeat; padding:10px 20px;text-indent:10px;text-align:left;color: red;'>  
+<center>
+                
+ <!--Dang ky tu van-->
+
+        <div class='f_dangkytuvan'>
+
+        	<div class='fdangkytuvan_center'>
+
+               
+                
+
+               <form action='https://docs.google.com/forms/d/e/1FAIpQLSc_ZQyHj1MhD0VieT3CxtTiz9jLH7dmIfP1NTKJw989CyLZyA/formResponse?embedded=true' class='fdangkytuvan_cl' id='form_dathang' method='POST' onsubmit='return emptycart()' target='hidden_iframe'>
+
+                   <br/> <br/>  <input class='input_dktuvan' id='entry_249843736' name='entry.249843736' placeholder='Họ và Tên' required='' type='text' value=''/>
+
+                     <br/> <br/>  <input class='input_dktuvan' id='entry_969980964' name='entry.969980964' placeholder='Số Điện Thoại' required='' type='number' value=''/>
+
+                     <br/>  <br/>   <input class='input_dktuvan' id='entry_145114698' name='entry.145114698' placeholder='E-Mail' required='' type='text' value=''/>
+
+					 <br/> <br/>	<input class='input_dktuvan' id='entry_704018011' name='entry.704018011' placeholder='Yêu Cầu ' required='' type='text' value=''/>
+
+                 <br/> <br/>
+<input class='btdangkytuvan' id='btdktuvan1' name='submit' type='submit' value='Gửi'/>
+                        
+
+  </form>
+<div class='hidden-message alert-box success' style='display: none;'>
+Cảm ơn quý khách đã gởi yêu cầu! Chúng tôi sẽ sớm liên lạc với quý khách trong thời gian sớm nhất.
+</div>
+<iframe id='hidden_iframe' name='hidden_iframe' onload='if(submitted){}' style='display: none;'/><script type='text/javascript'>   var submitted=false;    function emptycart(){       submitted=true;        $(&#39;#form_dathang&#39;).toggle();             $(&#39;.hidden-message&#39;).show();               } $(&quot;iframe#hidden_iframe&quot;).on(&#39;load&#39;, function() {     simpleCart.empty(); }); </script>
+<div class='hidden-message alert-box success' style='display: none;'>
+Cảm ơn quý khách đã gởi yêu cầu! Chúng tôi sẽ sớm liên lạc với quý khách trong thời gian sớm nhất.
+</div>
+<iframe id='hidden_iframe' name='hidden_iframe' onload='if(submitted){}' style='display: none;'/><script type='text/javascript'>   var submitted=false;    function emptycart(){       submitted=true;        $(&#39;#form_dathang&#39;).toggle();             $(&#39;.hidden-message&#39;).show();               } $(&quot;iframe#hidden_iframe&quot;).on(&#39;load&#39;, function() {     simpleCart.empty(); }); </script>
+            </div>
+
+            
+
+        </div>' width='600px'/><br/>Đất Nền Mặt Tiền Đường Hòa Bình</a>.Shophouse Richstar Novaland. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$('#myModal').linhnguyen($('#myModal').data());
+		}
+});
+' width='600px'/><br/>Đất Nền Mặt Tiền Đường Hòa Bình</a>.Shophouse Richstar Novaland. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
